598d4bae7a839a6e0d5e49e9e3863387a109deac
Joe Tustin <j.tustin@gmail.com>
2019-03-12 20:14:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -23,3 +23,22 @@ end
 def last_element(array)
   return array.last
 end 
+
+def first_element_with_array_methods(array)
+  return array.first
+end 
+
+def last_element_with_array_methods(array)
+  return array.last
+end 
+
+def length_of_array(array)
+  return array.length
+end
+
+
+
+
+
+
+
